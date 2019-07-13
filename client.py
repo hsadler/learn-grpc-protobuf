2ba2fb74ebbb9f001ff5b15ced5faaa55ea6712a
Harry Sadler <harrysadlermusic@gmail.com>
2019-07-13 00:46:20 -0700

    update README

--- client.py
+++ client.py
@@ -21,7 +21,7 @@ v = 16
 number = calculator_pb2.Number(value=v)
 # make call to server
 res = stub.SquareRoot(number)
-print('result of square_root(' + str(v) + '): ', res.value)
+print(f'result of square_root({v}): {res.value}')
 
 # TEST 'ADD' ENDPOINT
 
@@ -31,7 +31,7 @@ n2 = 5
 twoNumbers = calculator_pb2.TwoNumbers(n1=n1, n2=n2)
 # make call to server
 res = stub.Add(twoNumbers)
-print('result of add(' + str(n1) + ',' + str(n2) + '): ', res.value)
+print(f'result of add({n1}, {n2}): {res.value}')
 
 
 # create a 'hello' stub (client)
